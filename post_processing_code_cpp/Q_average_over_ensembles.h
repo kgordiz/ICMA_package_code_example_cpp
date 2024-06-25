7f45c9930f9112626658f6b12ab3a62d3074449d
Kiarash Gordiz <47131386+kgordiz@users.noreply.github.com>
2024-06-25 15:48:39 -0400

    Add files via upload

---
+++ post_processing_code_cpp/Q_average_over_ensembles.h
@@ -0,0 +1,13 @@
+
+#ifndef Q_AVERAGE_OVER_ENSEMBLES_H
+#define Q_AVERAGE_OVER_ENSEMBLES_H
+
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <math.h>
+
+void average_Q();
+
+#endif // Q_AVERAGE_OVER_ENSEMBLES_H
---
+++ post_processing_code_cpp/calc_go_contour_source.h
@@ -0,0 +1,14 @@
+
+#ifndef CALC_GO_CONTOUR_SOURCE_H
+#define CALC_GO_CONTOUR_SOURCE_H
+
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <math.h>
+#include <fftw3.h>
+
+void calculate_contour();
+
+#endif // CALC_GO_CONTOUR_SOURCE_H
